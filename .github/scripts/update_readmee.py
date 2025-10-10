5e46348cc2b78c5c53480d1ff74f1034e1feb8ff
Ramazan TÜFEKÇİ <4970844+ramazantufekci@users.noreply.github.com>
2025-10-10 14:19:33 +0300

    Update update_readmee.py

--- .github/scripts/update_readmee.py
+++ .github/scripts/update_readmee.py
@@ -1,10 +1,10 @@
 from datetime import datetime
 
-with open('README.template.md', 'r', encoding='utf-8') as f:
+with open('README.md', 'r', encoding='utf-8') as f:
     template = f.read()
 
 result = template.replace('{#formdate#}', datetime.now().strftime('%Y-%m-%d  %H:%M:%S'))
 
-'''with open('README.md', 'w', encoding='utf-8') as f:
+with open('README.md', 'w', encoding='utf-8') as f:
     f.write(result)
-'''
+
